6c0212e1f6066713e5e987877dcc9925e0fcc930
Zack Hutchens <zhutchens1@gmail.com>
2021-05-17 13:25:21 -0400

    testing with rf0673

--- grab_rass_images.py
+++ grab_rass_images.py
@@ -1,5 +1,5 @@
-from astroquery.skyview import SkyView as skyview
-
+from astroquery.skyview import SkyView as sv
+from astropy.io import fits
 
 def grab_images():
     """
@@ -11,3 +11,11 @@ def grab_images():
     Returns
     ------------------
     """
+    pass
+
+
+if __name__=='__main__':
+    # try for RESOLVE grp 836 (N=40 w/ central rf0673)
+    data = sv.get_images(position='181.1126055, 1.8961243', survey=['RASS Background 1', 'RASS-Cnt Soft'])
+    #data[0].writeto('rf0673_rassbg1.fits')
+    data[1].writeto('rf0673_rasscntsoft.fits')
