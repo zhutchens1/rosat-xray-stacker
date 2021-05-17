57df876227f333ea7b54df351db0fec52790a8e9
Zack Hutchens <zhutchens1@gmail.com>
2021-05-17 10:02:08 -0400

    outline main code

---
+++ rosat_xray_stacker.py
@@ -0,0 +1,30 @@
+"""
+Stack diffuse X-ray emission in galaxy groups from ROSAT All-Sky Survey Data.
+Author: Zack Hutchens (G3 subteam with Kelley Hess + Andrew Baker)
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from astroquery.skyview import SkyView as skyview
+
+def rosat_xray_stacker():
+    """
+    Stack RASS X-ray data for a sample of galaxy groups.
+
+    Parameters
+    -------------------
+    grpid : np.array
+       Galaxy group ID numbers.
+
+    Returns
+    -------------------
+    """
+   
+    # (1) Download X-ray images using SkyView
+
+    # (2) Image Inspection/Classification 
+
+    # (3) Image Stacking 
+
+    pass
