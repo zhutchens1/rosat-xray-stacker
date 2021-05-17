b5dd298217718e1a7ac9a53927c7b9a325d710fd
Zack Hutchens <zhutchens1@gmail.com>
2021-05-17 10:53:53 -0400

    init file for downloading images

---
+++ grab_rass_images.py
@@ -0,0 +1,13 @@
+from astroquery.skyview import SkyView as skyview
+
+
+def grab_images():
+    """
+    Extract group images from the ROSAT All-Sky Survey and save to local disk.
+
+    Parameters
+    ------------------
+
+    Returns
+    ------------------
+    """
