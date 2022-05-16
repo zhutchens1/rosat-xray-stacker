64c6637cb1bf6f7d750a238cb1eac152f58c6e0c
Zack Hutchens <zhutchens1@gmail.com>
2022-05-16 10:35:15 -0400

    initial commit

---
+++ mask_published_pt_sources.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io import fits
+
+def mask_sources(direc,sourcelist):
+    pass
+
+def mask_individual_image(img,sourcelist):
+    pass
+
+if __name__=='__main__':
+    pass
