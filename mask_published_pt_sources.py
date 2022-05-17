31f33256ae791a72435c41f3c8ce6188658c0e42
Zack Hutchens <zhutchens1@gmail.com>
2022-05-17 14:27:19 -0400

    outlining code for masking sources

--- mask_published_pt_sources.py
+++ mask_published_pt_sources.py
@@ -1,12 +1,40 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import astropy.io import fits
+import pandas as pd
+from astropy.wcs import WCS, utils as wcsutils
+from astropy.io import fits
+import astropy.units as uu
+from astropy.coordinates import SkyCoord
+from photutils.apertures import CircularAperture
 
-def mask_sources(direc,sourcelist):
-    pass
+def mask_individual_image(img_arr,wcs,ra,dec,extent):
+    # get range of RA/Dec to feed in 
+    in_image_sel = ...
+    source_in_image_skycoord = SkyCoord(ra=ra[in_image_sel]*uu.deg, dec=dec[in_image_sel]*uu.deg)
+    source_xpos, source_ypos = wcsutils.skycoord_to_pixel(source_in_image_skycoord, wcs)
+    xypos = np.array([source_xpos,source_ypos]).T
+    apertures = CircularAperture(xypos,r=extent[in_image_sel])
+    masks = apertures.to_mask(method='center')
+    newimage = np.zeros_like(img_arr)
+    final_mask = np.sum(np.array([msk.to_image(shape=((img_arr.shape[0],img_arr.shape[1]))) for msk in masks]),axis=0)
+    replacesel = (final_mask>0)
+    newimage[~replacesel]=image[~replacesel]
+    return newimage
 
-def mask_individual_image(img,sourcelist):
-    pass
+def generate_masked_images(indirec,outdirec,ra,dec,extent):
+    files = os.listdir(indirec)
+    for ff in files:
+        hdulist = fits.open(indrec+ff)
+        wcs = WCS(hdulist[0])
+        image = hdulist[0].data
+        masked_im = mask_individual_image(image,wcs,ra,dec,extent)
+        hdulist[0].data=masked_im
+        hdulist.writeto(outdirec+ff,overwrite=True)
+        hdulist.close()
 
 if __name__=='__main__':
-    pass
+    x = pd.read_hdf('rass2rxs.hdf5')
+    ra = np.array(x.ra)
+    dec = np.array(x.dec)
+    extent = np.array(x.source_extent)
+
+    
